fb885f24302bc7adf6839e69db3f6d938a477f62
Patrik <patrikmatos.tecinfo@gmail.com>
2020-01-03 18:22:05 -0300

    Utilizando a funçao map

--- array/src/App.js
+++ array/src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const posts = [
+    {id: 1, title: 'POST 1', content: 'Primeiro Post'},
+    {id: 2, title: 'POST 2', content: 'Primeiro Post'},
+    {id: 3, title: 'POST 3', content: 'Primeiro Post'},
+  ];
+
+  const conteudo = posts.map((post) => 
+    <div id={post.id}>
+      <h2>{post.title}</h2>
+      <p>{post.content}</p>
+    </div>
+  );
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {conteudo}
     </div>
   );
 }
